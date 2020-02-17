4aa83aba912b1741f93ea65fc234f5c5fe4287b3
Miguel Morujão <miguelmorujao@gmail.com>
2020-02-17 00:37:03 +0000

    Fix

--- imports/ui/Layers.jsx
+++ imports/ui/Layers.jsx
@@ -13,14 +13,14 @@ const Layers = (props) => {
                     position: index > 0 ? "absolute" : "relative" ,
                     zIndex: index, 
                     left: index > 0 ? scaleX(layers[0], canvas ,layer.position.x) : "",
-                    top: index > 0 ? scaleY(layers[0], canvas ,layer.position.y) : ""
+                    top: index > 0 ? scaleY(layers[0], canvas ,layer.position.y) : "",
                 }}>
                     <img 
                     key={layer.file}
                     src={`/images/${layer.file}`} 
                     className="image" 
                     style={{
-                        width: index > 0 ? scaleX(layers[0], canvas ,layer.imageData.width) : "100%",
+                        maxWidth: index > 0 ? scaleX(layers[0], canvas ,layer.imageData.width) : "100%",
                     }}
                     onLoad={(img) => index===0 && setCanvas(img.target)}/> 
                 </div>
